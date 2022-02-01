fadf69147d4ba3643877371a5533adda053e2e17
gtekle <teklegy@gmail.com>
2022-02-01 21:05:50 +0300

    Create GamesAPI class to fetch Games API data

--- src/modules/GamesAPI.js
+++ src/modules/GamesAPI.js
@@ -1,7 +1,28 @@
-const GAMES_API_BASE_URL = '';
+const GAMES_API_BASE_URL = 'https://mmo-games.p.rapidapi.com';
+const HOST = 'mmo-games.p.rapidapi.com';
+const API_KEY = 'dbb7593505mshdb2b499e2f3acf1p12c4c1jsn93ec8e29d6c6';
 
 class GamesApi {
   constructor() {
     this.games = [];
   }
+
+  async getGames() {
+    this.games.push(... await fetch(`${GAMES_API_BASE_URL}/games`,
+      {
+        headers: {
+          'x-rapidapi-host': HOST,
+          'x-rapidapi-key': API_KEY
+        }
+      })
+      .then((response) => response.json())
+      .then(data => data)
+      .catch((error) => error));
+    return this.games;
+  }
 }
+
+const gamesApi = new GamesApi();
+gamesApi.getGames();
+
+export default gamesApi;
