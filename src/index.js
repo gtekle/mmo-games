0a7e3e0be349e553fd4d370b0dde48dbe8e1d4b7
gtekle <teklegy@gmail.com>
2022-02-01 21:08:39 +0300

    Add basic styling to main section and game list item

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import './style.css';
 import ICON from './assets/img/logo.png';
+import gamesApi from './modules/GamesAPI.js';
+import renderGame from './components/GameUI.js';
 
 window.addEventListener('load', () => {
   const logoContainer = document.querySelector('.logo-container');
   const logoIcon = new Image();
   logoIcon.src = ICON;
-
   logoContainer.appendChild(logoIcon);
+
+  gamesApi.games.forEach((game) => {
+    if (game.id < 10) {
+      renderGame(game);
+    }
+  });
+
 });
