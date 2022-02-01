907568a0565b015d2ebadda11266289d2ceab3b9
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-02-01 17:26:50 +0200

    Add functionality to show pop up and delete it
    - Add one event listener "onload" to "hide" the display of the pop-up mark up
    - Add an event listener for a trial button to check pup up functionality
    - Add an event listener to close the pop up when "delete" button is clicked

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import './style.css';
 import ICON from './assets/img/logo.png';
+import deletePopUpIcon from './assets/img/icons/icons8-delete-64.png';
+
+const commentPopUpForm = document.querySelector('#comment-add-comment-form');
+const popUpTrialButton = document.querySelector('#comment-trial-button');
+const commentPopUpSection = document.querySelector('#comment-popup-section');
+const deletePopUpIconElementContainer = document.querySelector('#delete-popup');
+
+commentPopUpForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('test function to make comment call');
+  event.target.reset();
+});
+
+popUpTrialButton.addEventListener('click', () => {
+  const deleteIcon = new Image();
+  deleteIcon.src = deletePopUpIcon;
+  deletePopUpIconElementContainer.replaceChildren(deleteIcon);
+  commentPopUpSection.style.display = 'block';
+  // Implement hiding of other sections to only show pop up content
+});
+
+deletePopUpIconElementContainer.addEventListener('click', () => {
+  commentPopUpSection.style.display = 'none';
+})
 
 window.addEventListener('load', () => {
+  commentPopUpSection.style.display = 'none';
   const logoContainer = document.querySelector('.logo-container');
   const logoIcon = new Image();
   logoIcon.src = ICON;
