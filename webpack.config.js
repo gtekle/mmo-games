2afd2434821a6e1c260c6626dc3207b37dd43a85
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-02-02 15:42:25 +0200

    Update webpack.config.js
    - Initialise wepack to use the Dotenv webpack plugin to add env variables to the prod/dev bundle.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   mode: 'development',
@@ -22,6 +23,7 @@ module.exports = {
       title: 'mmo games',
       template: './src/index.html',
     }),
+    new Dotenv(),
   ],
   module: {
     rules: [
