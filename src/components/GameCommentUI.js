7f848ef9f1361d2713d8733d59d3bbbd6f628b65
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-02-03 07:45:48 +0200

    Update GameCommentUI.js
    Refine comment UI function to dynamically generate the HTML instead

--- src/components/GameCommentUI.js
+++ src/components/GameCommentUI.js
@@ -1,23 +1,47 @@
-const renderCommentsPopUp = (game) => {
-  const gameImageElement = document.querySelector('#game-image');
-  const nameOfGameElement = document.querySelector('#name-of-game');
-  const shortGameDescriptionElement = document.querySelector('#short-description');
-  const gameUrlElement = document.querySelector('#game-url');
-  const gameGenreElement = document.querySelector('#genre');
-  const gamePlatformElement = document.querySelector('#platform');
-  const gamePublisherElement = document.querySelector('#publisher');
-  const gameReleaseDateElement = document.querySelector('#release-date');
-  const gameDeveloperElement = document.querySelector('#game-developer');
+import deletePopUpIcon from '../assets/img/icons/icons8-delete-64.png';
 
-  gameImageElement.src = game.thumbnail;
-  nameOfGameElement.innerHTML = game.title;
-  shortGameDescriptionElement.innerHTML = game.short_description;
-  gameUrlElement.innerHTML = game.game_url;
-  gameGenreElement.innerHTML = game.genre;
-  gamePlatformElement.innerHTML = game.platform;
-  gamePublisherElement.innerHTML = game.publisher;
-  gameReleaseDateElement.innerHTML = game.release_date;
-  gameDeveloperElement.innerHTML = game.developer;
+const renderCommentsPopUp = (game) => {
+  const popUpContainer = document.querySelector('#comment-popup-container');
+  popUpContainer.innerHTML = `
+  <div class="image-thumbnail-delete-icon-container">
+      <img id='game-image' src=${game.thumbnail} alt="game-image-thumbnail" class="comment-game-image">
+      <div class="comment-delete-icon">
+      <img id="delete-popup" src=${deletePopUpIcon} alt="">
+      </div>
+    </div>
+    <h3 class="comment-name-of-game" id="name-of-game"> ${game.title}</h3>
+    <div class="comment-game-short-description-details"> Short Description: <span id="short-description">${game.short_description}</span></div>
+    <div class="comment-game-description-container">
+      <div class="comment-game-description-details"> Game developer: <span id="game-developer">${game.developer}</span></div>
+      <div class="comment-game-description-details"> Game Url: <span id="game-url"> <a href=${game.game_url}> ${game.game_url} </a></span></div>
+      <div class="comment-game-description-details"> Genre: <span id="genre">${game.genre}</span></div>
+      <div class="comment-game-description-details"> Platform: <span id="platform">${game.platform}</span></div>
+      <div class="comment-game-description-details"> Publisher: <span id="publisher">${game.publisher}</span></div>
+      <div class="comment-game-description-details"> Release - date: <span id="release-date">${game.release_date}</span></div>
+    </div>
+    <div class="comment-comments-container">
+      <div class="comment-server-comments-container">
+        <h3 class="comment-server-comments-header-text"> COMMENTS ("number of comments to be shown from server")</h3>
+        <div class="comment-server-comments">
+          <p class="comment-individual-server-comment"> 
+            <span>"Time of the comment "</span> 
+            <span> "Author of the comment"</span> 
+            <span>"Actual Comment"</span> 
+          </p>
+          <p class="comment-individual-server-comment"> 
+            <span>"Time of the comment -2 "</span> 
+            <span> "Author of the comment - 2"</span> 
+            <span>"Actual Comment - 2"</span> 
+          </p>
+        </div>
+      </div>
+      <h3 class="comment-comments-form-header-text"> Add a Comment </h3>
+      <form id="comment-add-comment-form" class="comment-add-comment-form" action="submit">
+        <input type="text" placeholder="Your Name" id="commentorsName" class="comment-input-text-name"/>
+        <textarea placeholder="Your Comment" id="commentorsComment" class="comment-input-text-comment"></textarea>
+        <button type="submit" class="comment-button-submit"> COMMENT </button>
+      </form>
+    </div>`;
 };
 
 export default renderCommentsPopUp;
